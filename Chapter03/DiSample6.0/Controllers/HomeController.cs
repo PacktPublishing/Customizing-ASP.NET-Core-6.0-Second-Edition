b31f625f15b52bf672f3f36103ec5811173730f2
JeurgenGutsch <juergen@gutsch-online.de>
2021-08-06 22:51:56 +0200

    update chapter 03

---
+++ Chapter03/DiSample6.0/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DiSample.Models;
+
+namespace DiSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IService _service;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IService service)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var persons = _service.AllPersons();
+            return View(persons);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
