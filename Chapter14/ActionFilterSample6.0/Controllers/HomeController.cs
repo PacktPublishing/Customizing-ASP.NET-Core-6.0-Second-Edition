c016d40e13007969799c1892d5477055d5977bc9
JeurgenGutsch <juergen@gutsch-online.de>
2021-11-14 19:49:54 +0100

    update chapter 14

---
+++ Chapter14/ActionFilterSample6.0/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ActionFilterSample.Models;
+
+namespace ActionFilterSample.Controllers;
+
+[ServiceFilter(typeof(LoggingActionFilter))]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
